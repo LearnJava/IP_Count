1c6abc96ce9b2ae0d2d6ad6fd058562c5276a893
konstantin <konstantin-spb@bk.ru>
2021-12-03 16:00:23 +0300

    256 files. Added BufferWriter

--- src/main/kotlin/ru/konstantin/FileWork.kt
+++ src/main/kotlin/ru/konstantin/FileWork.kt
@@ -1,6 +1,7 @@
 package ru.konstantin
 
 import kotlinx.coroutines.*
+import java.io.BufferedWriter
 import java.io.File
 import java.io.IOException
 import java.nio.file.Files
@@ -12,7 +13,7 @@ class FileWork {
     suspend fun getUniqIPs(path: String) {
         var counter: Long = 1
         val startDate = Date().time
-
+        val partOfIps = mutableListOf<String>()
 //        val ipList = mutableListOf<String>()
 
         val jobs = mutableListOf<Job>()
@@ -20,20 +21,32 @@ class FileWork {
 //        val fixedThread = newFixedThreadPoolContext(100_000, "myThread----------->")
 
         createNeededFiles(path)
+        var fileStreams = openAllFileStreams("$path/test/ip_group_files/")
+
         try {
             val sourceLine = Files.lines(Paths.get("$path\\ip_addresses"))
             sourceLine.forEach { ip: String ->
-//                ipList.add(ip)
-                val job = GlobalScope.launch(Dispatchers.IO) {
-                    addStringToFile("$path/test/ip_group_files/${ip.substringBefore(".")}.txt", ip)
-//                    println("Added to file $ip")
-                }
-                jobs.add(job)
+                partOfIps.add(ip)
+
 
                 if (counter % 1_000_000 == 0L) {
-
+                    for (index in partOfIps) {
+                        val job = GlobalScope.launch(Dispatchers.IO) {
+                            fileStreams[index.substringBefore(".").toInt()].appendLine(index)
+//                    addStringToFile("$path/test/ip_group_files/${ip.substringBefore(".")}.txt", ip)
+//                    println("Added to file $ip")
+                        }
+                        jobs.add(job)
+                    }
+                    while (jobs.any { !it.isCompleted }) {
+                        println("Ждём завершения всех работ -> ${(jobs.filter { !it.isCompleted }).count()}")
+                    }
+                    jobs.clear()
+                    fileStreams.forEach { it.close() }
                     println("Handled $counter ips")
 //                    ipList.clear()
+                    fileStreams = openAllFileStreams("$path/test/ip_group_files/")
+                    println("$counter time is ${Date().time - startDate}")
                 }
 
                 counter++
@@ -42,6 +55,8 @@ class FileWork {
             e.printStackTrace()
         } finally {
             jobs.joinAll()
+
+            fileStreams.forEach { it.close() }
             println("Whole time is ${Date().time - startDate}")
         }
     }
@@ -56,6 +71,14 @@ class FileWork {
             val file = File("$pathToFolder/test/ip_group_files/$index.txt")
             file.createNewFile()
         }
+    }
+
+    fun openAllFileStreams(pathToFolder: String): MutableList<BufferedWriter> {
+        val fileStreams = mutableListOf<BufferedWriter>()
+        for (index in 0..255) {
+            fileStreams.add(File("${pathToFolder}/$index.txt").bufferedWriter())
+        }
+        return fileStreams
     }
 }
 
